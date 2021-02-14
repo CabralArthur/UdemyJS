f079774aec5e215aaad42c03ec1fb2b675946ff4
Ah-Cabral <63428806+Ah-Cabral@users.noreply.github.com>
2021-02-14 18:25:07 -0300

    :up: O básico de Const, Let e Var.

---
+++ exercicios-js/fundamentos/2-const-let-var.js
@@ -0,0 +1,15 @@
+//Atribuição de variável (utilizando var)
+
+var a = 3; 
+
+//Tip: Utilize bons nomes nas suas variáveis.
+
+let b = 2; //Atribuição de variável (utilizando let)
+
+//No geral, evite utilizar var para criar variáveis.
+//Pelo fato de que let é mais moderno.
+
+//Atribuindo constante (utilizando const)
+const c = 1;x
+
+console.log(a,b,c)
