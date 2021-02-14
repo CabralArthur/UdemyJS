19f5ca91eec319ab23ca5895bae2cae2e64df760
Ah-Cabral <63428806+Ah-Cabral@users.noreply.github.com>
2021-02-14 17:57:05 -0300

    :up: Aula Organização JavaScript

---
+++ exercicios-js/fundamentos/organizacao.js
@@ -0,0 +1,28 @@
+```
+A pergunta dessa aula é: como JavaScript é organizado?
+
+Existem muitas formas de se organizar JavaScript, uma das formas mais
+características é inspirada na linguagem C.
+
+Do ponto de visto de organização básica da linguagem, ela é organizada em
+sentenças e blocos de código.
+
+Analogia: Como se organiza um livro de português?
+
+Letras --> Palavras --> Parágrafos --> Seções --> Capítulos --> Livros
+
+Ou seja, a lógica é a mesma, porém o modo em que o "autor" 
+do livro(ou nesse caso, do código) fará será variado.
+```
+
+console.log("Sentença de cógido");
+
+//Bloco de código
+
+{
+  //Chave de abertura
+  {
+    console.log("Olá");
+    console.log("Olá Mundo") //Padrão utilizado no curso
+  }
+} //Chave de Fechamento
