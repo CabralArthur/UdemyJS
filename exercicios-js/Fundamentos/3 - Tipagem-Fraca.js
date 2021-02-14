90e1dd61770e45dbb95c5bb2c938d7dc551bc8ad
Ah-Cabral <63428806+Ah-Cabral@users.noreply.github.com>
2021-02-14 18:31:23 -0300

    :up: Tipagem Fraca
    Aula que esclareceu que a Tipagem em JavaScript é facilmente mutável.

---
+++ exercicios-js/Fundamentos/3 - Tipagem-Fraca.js
@@ -0,0 +1,16 @@
+//Criando primeira variável do tipo string
+let qualquer = "Legal";
+
+//Printando variável e tipo
+console.log(qualquer);
+console.log(typeof qualquer);
+
+//Provando que a mesma variável pode ter diferentes tipos
+qualquer = 3.1516;
+
+//Printando variável e tipo
+console.log(qualquer);
+console.log(typeof qualquer);
+
+//Tip: Evite nomes genéricos e siglas para suas variáveis
+//     Seja objetivo!
