a0860c42dd95d0190fb2f177e8390084d89c3543
Ah-Cabral <63428806+Ah-Cabral@users.noreply.github.com>
2021-02-15 23:34:34 -0300

    :up: Classe Math

---
+++ exercicios-js/Fundamentos/6 - Math.js
@@ -0,0 +1,24 @@
+//Aula breve sobre a classe Math
+
+//Declarando constante de um raio de circunferência
+const raio = 5.6; 
+
+//Declarando constante referente à area de uma circunferência
+const area = Math.PI * Math.pow(raio, 2); //PI é uma constante, já Math.pow é uma função que calcula a potência
+
+//Dissecando a expressão:
+const pi = Math.PI;
+
+const pot = Math.pow(raio, 2)
+
+//Printando a constante pi
+console.log(pi);
+
+//Printando a potência
+console.log(pot)
+
+//Por fim, printando a área
+console.log(area);
+
+//Math é um objeto
+console.log(typeof Math);
