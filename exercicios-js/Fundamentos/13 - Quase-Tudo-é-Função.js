0f24315bb44f2ce051a39e13661b4bd7b5e2a5c1
Ah-Cabral <63428806+Ah-Cabral@users.noreply.github.com>
2021-02-16 11:47:06 -0300

    :up: Quase Tudo é Função!

---
+++ exercicios-js/Fundamentos/13 - Quase-Tudo-é-Função.js
@@ -0,0 +1,16 @@
+//O fato de você entender os princípios das coisas
+//faz com que você tenha uma visão mais assertiva sobre o que
+//você faz naturalmente.
+
+//Nessa caso falaremos de funções, se JS fosse um país, o presidente
+//seria a função, pelo fato dela ser essencial em quase tudo na linguagem.
+
+//Exemplos:
+
+console.log(typeof Object); //O Objeto em JS é uma função.
+
+class Produto{}; 
+
+console.log(typeof Produto); //Uma classe em JS também é uma função.
+
+//Como podemos ver, JavaScript é altamente centrado em funções.
