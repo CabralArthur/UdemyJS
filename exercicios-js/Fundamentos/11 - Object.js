2f1b48b225427e8b1ee3f62cf8b1a6a5cd175223
Ah-Cabral <63428806+Ah-Cabral@users.noreply.github.com>
2021-02-16 10:10:07 -0300

    :up: Objects

---
+++ exercicios-js/Fundamentos/11 - Object.js
@@ -0,0 +1,23 @@
+//Um objeto em JavaScript não é JSON (JavaScript Object Notation).
+
+//JSON É um formato textual.
+
+//Um par de chaves representa um objeto.
+const prod1 = {}; 
+
+//Adicionando dinamicamente uma CHAVE nome com VALOR Celular.
+prod1.nome = "Celular"; 
+
+//Adicionando dinamicamente uma CHAVE preco com VALOR Celular.
+prod1.preco = 4900;
+
+//Printando Objeto
+console.log(prod1);
+ 
+//Declarando constante do objeto produto 2
+const prod2 = {
+    nome: 'Camisa Polo',
+    preco: 79.90
+};
+
+console.log(prod2);
