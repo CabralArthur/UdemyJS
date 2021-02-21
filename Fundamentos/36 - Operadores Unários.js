d27014d6987796d8cf5aa274701eefe48cf95d41
Ah-Cabral <63428806+Ah-Cabral@users.noreply.github.com>
2021-02-21 17:20:28 -0300

    :up: Operadores Unários

---
+++ Fundamentos/36 - Operadores Unários.js
@@ -0,0 +1,12 @@
+let num1 = 1;
+let num2 = 2;
+
+//Forma pós-fixada
+num1++; //O Resultado aqui será 2
+console.log(num1);
+ 
+//Forma pré-fixada
+--num2; //O Resultado aqui será 1
+console.log(num2);
+
+//Tip: Tente não misturar vários operadores no seu código!
