2137b40fddcedf04732c9840cabc4ee346a44f27
Ah-Cabral <63428806+Ah-Cabral@users.noreply.github.com>
2021-02-21 16:58:59 -0300

    :up: Operadores Lógicos - Explicação Comentada

---
+++ Fundamentos/34 - Operadores Lógicos.js
@@ -0,0 +1,33 @@
+```
+Para falar sobre operadores lógicos, deve-se
+falar também de tabela verdade:
+
+Tabela Verdade do 'e' (&&):
+
+V e V -> V
+V e F -> V
+F e ? -> F
+
+Ou seja, para que o resultado final ocorra, todos
+os valores devem ser verdadeiros.
+
+Tabela Verdade do 'ou' (||):
+
+V ou ? -> V
+F ou V -> V
+F ou F -> F
+
+Ou seja, para que o resultado final ocorra
+no mínimo um valor deve ser verdadeiro.
+
+Tabela Verdade do 'ou' exclusivo (xor):
+
+V xor V -> F
+V xor F -> V
+F xor V -> V
+F xor F -> F
+
+Nesse caso, devem-se ter valores inversos juntos,
+para que o resultado final aconteça.
+
+```
