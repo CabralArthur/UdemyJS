a51b12b5a30752f6a5872f75a75e919ae791bab4
Ah-Cabral <63428806+Ah-Cabral@users.noreply.github.com>
2021-02-21 13:41:38 -0300

    :up: Operadores Aritméticos

---
+++ Fundamentos/32 - Aritméticos.js
@@ -0,0 +1,14 @@
+//Os operadores possuem suas formas de classificação,
+//nessa aula veremos os operadores aritméricos
+
+const [a,b,c,d] = [3, 5, 1, 15]; //Declarando 4 variáveis utilizando o destructuring
+
+const soma = a + b + c + d; //sintaxe infix (operador binário = operador que atua em 2 operandos)
+
+const subt = d - b;
+
+const mult = a * b;
+
+const modulo = a % 2;
+
+console.log(soma, subt, mult, modulo);
