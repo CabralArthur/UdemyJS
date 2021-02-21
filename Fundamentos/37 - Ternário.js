a9e6a33f0c22bdec5150c477f426464b6d753028
Ah-Cabral <63428806+Ah-Cabral@users.noreply.github.com>
2021-02-21 17:27:29 -0300

    :up: Operador Ternário

---
+++ Fundamentos/37 - Ternário.js
@@ -0,0 +1,10 @@
+//Aula sobre operador ternário (utiliza 3 operandos).
+                            //Expressão Relacional
+const resultado = nota => nota >= 7 ? 'Aprovado' : 'Reprovado';
+                                    //Result. (V)   //Result. (F)
+
+//Resultado para Aprovação
+console.log(resultado(7.1));
+
+//Resultado para Reprovação
+console.log(resultado(6));
