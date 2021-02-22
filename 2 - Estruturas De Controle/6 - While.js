1270afc54d6d80f80f6af0bc44d844d509770670
Ah-Cabral <63428806+Ah-Cabral@users.noreply.github.com>
2021-02-22 10:52:03 -0300

    :up: Aula While

---
+++ 2 - Estruturas De Controle/6 - While.js
@@ -0,0 +1,27 @@
+//Criando função que retornará um valor aleatório no intervalo entre min e max.
+function getInteiroAleatorioEntre(min,max){
+    const valor = Math.random() * (max - min) + min;
+    return Math.floor(valor)
+}
+
+let opcao = 0;
+let c = 0;
+//Enquanto o valor da opção for diferente de -1
+
+while(opcao != -1){ 
+    //[Adicionando um contador, exemplo adicional feito por mim]
+
+    //O número continuará sendo sorteado.
+    opcao = getInteiroAleatorioEntre(-1, 10);
+
+    //E o incremento vai sendo feito. (Incremento = Operador Unário já visto)
+    c++; 
+
+    //E o número sorteado atual será impresso.
+    console.log('Opção Sorteada foi: ' + opcao)
+};
+
+console.log('Sorteios Necessários para se chegar ao número final: ' + c)
+
+//Ao fim da Estrutura de Repetição, ele imprimirá essa mensagem.
+console.log('Fim do Código'); 
