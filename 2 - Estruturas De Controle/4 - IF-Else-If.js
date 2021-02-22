98ce1accd5c07bf646d1d7ca03e46d564b692e8d
Ah-Cabral <63428806+Ah-Cabral@users.noreply.github.com>
2021-02-22 10:19:04 -0300

    :up: Aula If - Else if

---
+++ 2 - Estruturas De Controle/4 - IF-Else-If.js
@@ -0,0 +1,24 @@
+Number.prototype.entre = function (inicio,fim){
+    return this>= inicio && this <=fim
+}
+
+const imprimirResultado = function(nota){
+    if (nota.entre(9,10)){
+        console.log('Quadro de Honra');
+    } else if(nota.entre(7,8.99)){
+        console.log('Aprovado');
+    } else if(nota.entre(4,6.99)){
+        console.log('Recuperação');
+    } else if(nota.entre(0,3.99)){
+        console.log('Reprovado');
+    } else{
+        console.log('Nota Inválida')
+    };
+
+    console.log('Fim');
+};
+
+imprimirResultado(10);
+imprimirResultado(8.6);
+imprimirResultado('Ui');
+imprimirResultado(2.5);
