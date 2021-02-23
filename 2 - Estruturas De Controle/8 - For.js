321c7c36fcb0890cb13e884c00f96798a9b332e9
Ah-Cabral <63428806+Ah-Cabral@users.noreply.github.com>
2021-02-23 11:53:13 -0300

    :up: Estrutura For

---
+++ 2 - Estruturas De Controle/8 - For.js
@@ -0,0 +1,18 @@
+
+//Declarando contador (variável que irá controlar o laço)
+let contador = 1;
+
+while(contador <=10 ){
+    console.log(contador);
+    contador++;
+};
+
+for(let i = 1; i<=10; i++){
+    console.log(i)
+};
+
+const notas = [6.7, 7.4, 9.8, 8.1, 7.7];
+
+for(let i = 0; i < notas.length; i++){
+    console.log(notas[i]);
+}
