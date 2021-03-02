5f06bdc16e049dc364ecf0ded0725b5cf8641423
AhCabral <arthurcabral.dev@gmail.com>
2021-03-01 23:39:28 -0300

    Aula 2 Funções Factory

---
+++ 3 - Funções/20 - Compara-Classe-e-Factory.js
@@ -0,0 +1,27 @@
+//Aula comparando função factory com classe
+
+//Criando classe Pessoa
+class Pessoa {
+    //Construtor da classe
+    constructor(nome) {
+        this.nome = nome
+    }
+
+    falar() {
+        console.log(`Meu nome é ${this.nome}`)
+    }
+}
+
+const p1 = new Pessoa('João')
+p1.falar()
+
+//Criando função que irá criar uma pessoa
+const criarPessoa = nome => {
+    return {
+        falar: () => console.log(`Meu nome é ${nome}`)
+    }
+}
+
+const p2 = criarPessoa('João')
+
+p2.falar()
