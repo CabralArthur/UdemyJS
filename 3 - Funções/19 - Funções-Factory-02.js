dbe641fe39fbb28e339df3bbf93d6d38331ad013
AhCabral <arthurcabral.dev@gmail.com>
2021-03-01 22:55:50 -0300

    Aula Funções Factory

---
+++ 3 - Funções/19 - Funções-Factory-02.js
@@ -0,0 +1,12 @@
+//Exemplo Antes da aula
+
+
+function criarProduto(nomeDoProduto, precoDoProduto) {
+    return {
+        nome: nomeDoProduto,
+        preco: precoDoProduto
+    };
+
+};
+
+console.log(criarProduto('Celular', "R$ 10,00"));
