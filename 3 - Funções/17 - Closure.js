4934867e3ed03a34f9c8226966fc4d2f30edf030
AhCabral <arthurcabral.dev@gmail.com>
2021-03-01 22:41:51 -0300

    :up: Comentários Closure

---
+++ 3 - Funções/17 - Closure.js
@@ -0,0 +1,6 @@
+//Closure é o excomo criado quando uma função é declarada
+
+//Esse escopo irá permitir que você acesse e manipule
+//as variáveis as variáveis externas à função
+
+//Contexto Léxico em ação
