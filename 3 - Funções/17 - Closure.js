d131e9ccf14ed16ce0a4d2c0a1d022b6beba9487
AhCabral <arthurcabral.dev@gmail.com>
2021-03-01 22:49:09 -0300

    :up: Aula Closures

--- 3 - Funções/17 - Closure.js
+++ 3 - Funções/17 - Closure.js
@@ -4,3 +4,18 @@
 //as variáveis as variáveis externas à função
 
 //Contexto Léxico em ação
+
+const g = 'Global';
+
+function fora() {
+    //Mesmo com uma variável previamente declarada acima
+    const g = 'Local';
+    //A função e suas variáveis entendem suas 'origens'
+    function dentro() {
+        return g;
+    };
+    return dentro;
+};
+
+const minhaFuncao = fora(); //Retornará a local
+console.log(minhaFuncao()) 
