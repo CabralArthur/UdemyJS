179b5770c85810ca5da188ed0bd102595a64bdaa
AhCabral <arthurcabral.dev@gmail.com>
2021-03-03 00:01:24 -0300

    Exercício 08 - Basquete - Enunciado

---
+++ 4 - Exercícios Funções/Exe08.js
@@ -0,0 +1,16 @@
+/*
+
+Pedro joga N jogos de basquete por temporada.
+
+Para saber como está ele está progredindo, ele mantém
+registro de todos os as pontuações feitas por jogo.
+
+Após cada jogo ele anota no novo valor e confere se o
+mesmo é maior ou menor que seu melhor e pior desempenho.
+
+Dada uma lista string = “pontuação1 pontuação2
+pontuação3 etc..”, escreva uma função que ao recebê-la irá comparar os valores um a um e irá retornar um
+vetor com o número de vezes que ele bateu seu recorde de maior número de pontos e quando fez seu pior
+jogo. (Número do pior jogo)
+
+*/
