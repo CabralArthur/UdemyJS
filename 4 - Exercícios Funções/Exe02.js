883d5a8f67d0e64e01f78faa22a8520f66a79c85
AhCabral <arthurcabral.dev@gmail.com>
2021-03-02 23:46:19 -0300

    Exercício 02 - Triângulos (Comentado)

--- 4 - Exercícios Funções/Exe02.js
+++ 4 - Exercícios Funções/Exe02.js
@@ -13,12 +13,25 @@ lados de um triângulo e retorne sua classificação quanto ao tamanho de seus l
 
 function checaTriangulo(lado1 = 1, lado2 = 1, lado3 = 1) {
 
+    //Nesse caso, se todos os lados foram iguais
     if (lado1 == lado2 && lado1 == lado3 && lado2 == lado3) {
+
+        //Faz-se a impressão da mensagem informando que ele é equilátero
+
         console.log('O triângulo em questão é equilátero')
+
+        //Se não, se no mínimo 2 lados forem iguais
     } else if (lado1 == lado2 || lado1 == lado3 || lado2 == lado3) {
-        console.log('O triângulo em questão é isóceles')
+
+        //Faz-se a impressão da mensagem informando que ele é isósceles
+        console.log('O triângulo em questão é isósceles')
+
+        //Se não, se todos os lados forem diferentes
     } else if (lado1 != lado2 && lado1 != lado3 && lado2 != lado3) {
+
+        //Faz-se a impressão da mensagem informando que ele é escaleno
         console.log('O triângulo em questão é escaleno')
+
     } else {
         console.log('Inválido')
     }
