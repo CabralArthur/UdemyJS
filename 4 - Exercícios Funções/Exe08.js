95b90027ff7849c31ab4697b852b17c4989e6fb6
AhCabral <arthurcabral.dev@gmail.com>
2021-03-04 20:56:51 -0300

    :up: Ideia 01

--- 4 - Exercícios Funções/Exe08.js
+++ 4 - Exercícios Funções/Exe08.js
@@ -2,7 +2,7 @@
 
 Pedro joga N jogos de basquete por temporada.
 
-Para saber como está ele está progredindo, ele mantém
+Para saber como está progredindo, ele mantém
 registro de todos os as pontuações feitas por jogo.
 
 Após cada jogo ele anota no novo valor e confere se o
@@ -14,3 +14,45 @@ vetor com o número de vezes que ele bateu seu recorde de maior número de ponto
 jogo. (Número do pior jogo)
 
 */
+
+n = [1, 800, 500];
+
+function comparaJogos(list) {
+    //Criando variável referente ao pior jogo, que armazena inicialmente uma variável indefinida
+    let menorNum = undefined;
+
+    //Criando variável referente à maior pontuação, que armazena inicialmente uma variável indefinida
+    let maiorPonto = undefined;
+
+    //Variável que irá armazenar a quantidade de vezes que o recorde foi batido
+    let record = 0;
+
+    for (var i = 0; i < list.length; i++) {
+
+        menorNum = list[0];
+
+        maiorPonto = list[i];
+
+        if (menorNum > list[i + 1]) {
+
+            menorNum = list[i + 1];
+
+        }
+
+        if (maiorPonto < list[i + 1]) {
+
+            maiorPonto = list[i + 1];
+
+            record++;
+        };
+
+    };
+
+    console.log(record);
+    console.log(maiorPonto);
+    console.log(menorNum);
+
+
+};
+
+comparaJogos(n);
