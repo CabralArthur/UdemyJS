7165799950d2e0a9274874dc933c83c4c923c62b
Ah-Cabral <63428806+Ah-Cabral@users.noreply.github.com>
2021-03-09 12:33:29 -0300

    Exercício 10 - É divisível ?

---
+++ 4 - Exercícios Funções/Exe10.js
@@ -0,0 +1,16 @@
+/*
+Crie uma função que verifica se um número inteiro 
+passado como parâmetro é divisível por 3 e retorne true ou false
+ */
+
+function eDiv(num){
+    if(num % 3 == 0){
+        console.log(true)
+    }else{
+        console.log(false)
+    }
+}
+
+eDiv(3)
+
+eDiv(1)
