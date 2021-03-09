31a50102cf94cba41e2f1ca0c2621e004ae92e47
Ah-Cabral <63428806+Ah-Cabral@users.noreply.github.com>
2021-03-09 17:27:35 -0300

    Exercício 11 - É bissexto ? [Comentado]

--- 4 - Exercícios Funções/Exe11.js
+++ 4 - Exercícios Funções/Exe11.js
@@ -16,16 +16,29 @@ mensagem e retornando true ou false.
 */
 
 function eBissexto(ano){
+    //Variável referente ao ano bissexto, inicialmente falso
     bissexto = false;
+
+    //Se o valor do ano for divisível por 400, ou se o valor do ano não for divisível por 100 e divisível por 4
     if(ano % 400 == 0 || (ano % 100 != 0 && ano % 4 == 0)){
+
+        //O ano em questão é bissexto
         console.log('O ano é bissexto');
+        //E o valor do bissexto passa a ser true
         bissexto = true;
+
     }else{
+
+        //Caso não, o ano em questão não é bissexto   
         console.log('O ano não é bissexto');
+        //E o valor do bissexto passa a ser false
         bissexto = false;
+
     };
 };
 
+//Utilizando a função com o ano 2016 (Bissexto)
 eBissexto(2016);
 
+//Utilizando a função com o ano 2017 (Não bissexto)
 eBissexto(2017);
