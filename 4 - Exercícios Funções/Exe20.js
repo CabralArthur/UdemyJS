c3b7e2869fb068d02c91ecf63564913fed822969
AhCabral <arthurcabral.dev@gmail.com>
2021-03-10 17:18:44 -0300

    Exercício 20 - Conta até 50

---
+++ 4 - Exercícios Funções/Exe20.js
@@ -0,0 +1,11 @@
+/*
+
+Escrever um programa para exibir os números de 1 até 50 na tela
+
+*/
+
+(function conta() {
+    for (i = 0; i <= 50; i++) {
+        console.log(i)
+    }
+})()// --> Ivocando função
