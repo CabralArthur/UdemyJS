4028121f738c5010f61afa50aa9ab007ba72f1a6
Ah-Cabral <63428806+Ah-Cabral@users.noreply.github.com>
2021-03-10 18:47:27 -0300

    Exercício 17 - Enunciado

---
+++ 4 - Exercícios Funções/Exe17.js
@@ -0,0 +1,17 @@
+/*
+
+Um funcionário irá receber um aumento de acordo 
+com o seu plano de trabalho, de acordo com a tabela abaixo:
+
+Plano Aumento
+A      10%
+B      15%
+C      20%
+
+Faça uma função que leia o plano de trabalho 
+e o salário atual de um funcionário e calcula e imprime o seu novo salário. 
+
+Use a estrutura switch e faça um caso default que indique que o plano é inválido.
+
+
+*/
