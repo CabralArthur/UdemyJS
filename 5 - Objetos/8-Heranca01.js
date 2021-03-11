1c32b7699d996cb739478127fc922c999fffe228
Ah-Cabral <63428806+Ah-Cabral@users.noreply.github.com>
2021-03-11 14:37:23 -0300

    Aula 8 - Herança

--- 5 - Objetos/8-Heranca01.js
+++ 5 - Objetos/8-Heranca01.js
@@ -14,14 +14,25 @@ const volvo = {
     velMax: 200
 }
 
-console.log(ferrari.__proto__)
+//Um objeto não possui o atributo prototype
+console.log(ferrari.prototype)
 
+//Quando utiliza-se esse atributo, é possível acessar o objeto pai
+console.log(ferrari.__proto__) //Um objeto vazio
+
+//Um objeto criado aponta para Object.prototype
 console.log(ferrari.__proto__ === Object.prototype)
 
+//A mesma coisa para o volvo
 console.log(volvo.__proto__ === Object.prototype)
 
+//O Object.prototype não possui um objeto pai, e possui valor nulo
 console.log(Object.prototype.__proto__ === null)
 
 function meuObjeto() { }
+
+//Object e o meuObjeto são funções
 console.log(typeof Object, typeof meuObjeto)
+
+//Object.prototype é um objeto vazio, bem como meuObjeto, que foi criado com a função
 console.log(Object.prototype, meuObjeto.prototype)
